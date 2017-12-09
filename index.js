145143dab2ee25998b234b454361b68450bff6c8
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-09 16:12:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,16 +85,21 @@ function createRock(x) {
      */
      if ( checkCollision(rock) === true ) {
        endGame();
+     } else {
+       /**
+        * Otherwise, if the rock hasn't reached the bottom of
+        * the GAME, we want to move it again.
+        */
+       if ( rock.style.top < 360 ) {
+         moveRock();
+       } else {
+         /**
+          * But if the rock *has* reached the bottom of the GAME,
+          * we should remove the rock from the DOM
+          */
+          
+       }
      }
-    /**
-     * Otherwise, if the rock hasn't reached the bottom of
-     * the GAME, we want to move it again.
-     */
-
-    /**
-     * But if the rock *has* reached the bottom of the GAME,
-     * we should remove the rock from the DOM
-     */
   }
 
   // We should kick of the animation of the rock around here
