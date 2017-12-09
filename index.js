52240a4d36b856f2f279183795cca846281d5756
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-09 14:55:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-     if ( checkCollision() === true ) {
+     if ( checkCollision(rock) === true ) {
        endGame();
      }
     /**
